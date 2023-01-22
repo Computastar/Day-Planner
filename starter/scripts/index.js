014e3714fe8ab54d9937c240cbf5c987a6bf3c85
Computastar <jason.hunter@computastar.com>
2023-01-22 15:06:24 +0000

    Added daysjs library to index.html, added current day to index.js

--- starter/scripts/index.js
+++ starter/scripts/index.js
@@ -1,5 +1,8 @@
 // Add current Day time to header
 
+var today = dayjs();
+$("#currentDay").text(today.format('dddd, D MMMM  YYYY'));
+
 // Add timeBlock - use day.js library much easier than moment.js - Im all about the easiness
 
 // Save the time block
