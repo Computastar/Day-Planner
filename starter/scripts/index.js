c965f0366336a2441e9bc4a3876d6ac03d08437e
Computastar <jason.hunter@computastar.com>
2023-01-22 15:26:41 +0000

    Add timeBlock function to apply class and therefore styling

--- starter/scripts/index.js
+++ starter/scripts/index.js
@@ -5,4 +5,35 @@ $("#currentDay").text(today.format('dddd, D MMMM  YYYY'));
 
 // Add timeBlock - use day.js library much easier than moment.js - Im all about the easiness
 
+function timeBlock() {   
+    $(".time-block").each(function(){
+        var currentHour = parseInt($(this).attr("id").split("hour-")[1]);
+
+        console.log(currentHour);
+  
+        if (currentHour === dayjs().hour()){
+          console.log("timeblock is present")
+          $(this).addClass("present");
+          $(this).removeClass("past");
+          $(this).removeClass("future");      
+        }
+        if (currentHour < dayjs().hour()){
+          console.log("timeblock is past")
+          $(this).addClass("past");
+          $(this).removeClass("present");
+          $(this).removeClass("future");      
+        }
+        if (currentHour > dayjs().hour()){
+          console.log("timeblock is future")
+          $(this).addClass("future");
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+        }
+    
+    })}
+
+
 // Save the time block
+
+
+timeBlock();
